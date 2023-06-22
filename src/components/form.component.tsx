ad909c30d6adc7708c9f72e125f40301ef5c5fec
Sergio Benítez <sergio.kun21@gmail.com>
2023-06-22 15:05:41 -0500

    FE: add input and form component skeleton.

--- src/components/form.component.tsx
+++ src/components/form.component.tsx
@@ -1,1 +1,43 @@
 // Form component
+import React, { Reducer, useContext, useEffect, useReducer } from "react";
+
+const identity = <T,>(x: T) => x
+
+type FormCtx = {
+    loading: boolean
+    error: boolean
+    submit: () => void
+    reset: () => void
+    getValue: (key: string) => unknown
+    setValue: (key: string, val: unknown) => void
+    getError: (key: string) => boolean | string
+    setError: (key: string, val: boolean | string) => void
+}
+
+const FormContext = React.createContext<FormCtx | null>(null)
+
+export const useFormState = () => useContext(FormContext)
+
+type FormProps<T> = {
+    children: React.ReactNode
+    cleanOnChange?: boolean
+    initialState: T
+    schema?: Schema<T> // TODO: create Scheme type
+    onChange?: (data: T) => void
+    onError?: (errors: ErrorsOf<T>) => void // TODO: create ErrorsOf type
+    onSubmit?: (data: T) => void
+}
+
+const Form = <T extends Record<string, unknown>>({
+    children,
+    cleanOnChange,
+    initialState,
+    schema,
+    onChange,
+    onError,
+    onSubmit,
+}: FormProps<T>) => {
+    // TODO: Add body implementation
+}
+
+export default Form
--- src/components/input.component.tsx
+++ src/components/input.component.tsx
@@ -1,1 +1,21 @@
 // Input component
+
+export interface InputProps {
+    id: string
+    label: string
+    name?: string
+    formatter: Formatter // TODO: create a Formatter type
+    type?: string
+}
+
+const Input = ({
+    id,
+    label,
+    name = id,
+    formatter = Iso.id(), // TODO: Check ISO type
+    type = "text"
+}: InputProps) => {
+    // TODO: add component body
+}
+
+export default Input
